947a44a699d79394162841aa7cee597ccbbe39d2
sanchesaline6 <sanches.aline6@gmail.com>
2023-05-18 22:24:33 -0300

    String Operations Task Continued

--- VarTest/src/StringOperations.java
+++ VarTest/src/StringOperations.java
@@ -8,5 +8,24 @@ public class StringOperations {
         System.out.println("str1 length: " + str1.length());
         System.out.println("substring str3: " + str3.substring(0, 5));
         System.out.println("str2 uppercase:" + str2.toUpperCase());
+
+        if(str1.compareTo(str2) != 0){
+            if(str1.compareTo(str2) > 0){
+                System.out.println("str1 is larger than str2");
+            }
+            else {
+                System.out.println("str1 is less than str2");
+            }
+        }
+        else {
+            System.out.println("str1 is equal to str2");
+        }
+
+        if(str2.equals(str3)){
+            System.out.println("str2 is equal to str3");
+        }
+        else{
+            System.out.println("str2 is not equal to str3");
+        }
     }
 }
