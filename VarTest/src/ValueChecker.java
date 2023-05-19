107a43082046e8ec4ce73012b688f22788e73fcd
sanchesaline6 <sanches.aline6@gmail.com>
2023-05-19 11:24:25 -0300

    Loops Task

--- VarTest/src/ValueChecker.java
+++ VarTest/src/ValueChecker.java
@@ -6,17 +6,22 @@ public class ValueChecker {
 
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        intValue = in.nextInt();
+        do{
+            System.out.print("Enter a number: ");
+            intValue = in.nextInt();
 
-        if(intValue == 7){
-            System.out.println("That's lucky!");
-        }
-        else if(intValue == 13){
-            System.out.println("That's unlucky!");
-        }
-        else {
-            System.out.println("That number is neither lucky nor unlucky!");
-        }
+            if(intValue == 7){
+                System.out.println("That's lucky!");
+            }
+            else if(intValue == 13){
+                System.out.println("That's unlucky!");
+            }
+            else if(intValue != 0){
+                System.out.println("That number is neither lucky nor unlucky!");
+            }
+
+        }while(intValue != 0);
+
+
     }
 }
