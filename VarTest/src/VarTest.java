8820b3c2e622ed849f06e5d46f18ea729485e0a0
sanchesaline6 <sanches.aline6@gmail.com>
2023-05-18 21:42:46 -0300

    Declaring Variables and Using Literals Task

---
+++ VarTest/src/VarTest.java
@@ -0,0 +1,14 @@
+public class VarTest {
+    public static void main(String[] args) {
+
+        int iNum = 25;
+        char cVal = 'B';
+        boolean bVal = true;
+
+        System.out.println("iNum value: " + iNum);
+        System.out.println("cVal value: " + cVal);
+        System.out.println("bVal value: " + bVal);
+
+    }
+
+}
